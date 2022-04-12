00297918c425e9f6f5c5e99ff4b0703677330fc9
long.truong <long.truong@linecorp.com>
2022-04-12 15:42:44 +0700

    change profile picture

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Long20014';
+const name = 'Doc Templates';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
