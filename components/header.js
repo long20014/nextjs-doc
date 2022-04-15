f02963da824328fb7b4fc8e1f73278e41218da97
long.truong <long.truong@linecorp.com>
2022-04-15 15:15:09 +0700

    add title and logo to header

--- components/header.js
+++ components/header.js
@@ -1,10 +1,12 @@
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import navbarData from '../built-data/navbar.json';
+import headerData from '../fetched-data/navbar-data.json';
 import ActiveLink from './active-link';
 import constants from '../utils/constants';
 const { NAVBAR } = constants;
 const { navbarItems } = navbarData;
+const { config: navbarConfig } = headerData;
 
 const styles = {
   headerStyle: {
@@ -19,14 +21,40 @@ const styles = {
 function NormalLink({ children, href }) {
   return (
     <Link href={href}>
-      <a>{children}</a>
+      <a
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          textDecoration: 'none',
+          color: 'black',
+        }}
+      >
+        {children}
+      </a>
     </Link>
   );
+}
+
+function Logo({ src }) {
+  if (src.startsWith('img')) {
+    const localSrc = src.replace('img', '/images');
+    return <img src={localSrc} height="42" style={{ marginRight: '5px' }} />;
+  }
+  return <img src={src} height="42" style={{ marginRight: '5px' }} />;
+}
+
+function Title({ title }) {
+  return <div style={{ marginRight: '2rem' }}>{title}</div>;
 }
 
 export default function Header() {
   return (
     <div style={styles.headerStyle}>
+      <NormalLink href="/">
+        <Logo src={navbarConfig.logo.src} />
+        <Title title={navbarConfig.title} />
+      </NormalLink>
+
       {navbarItems.map((item) => {
         return (
           <ActiveLink
