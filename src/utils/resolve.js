deaaa1b20d0af90f42c57941dbf6d2070f028f3d
long.truong <long.truong@linecorp.com>
2022-05-06 11:53:48 +0700

    improve resolve lang path

--- src/utils/resolve.js
+++ src/utils/resolve.js
@@ -2,9 +2,9 @@ export const resolveLangPath = (path) => {
   if (path.split('/')[2]) {
     return path.split('/')[2];
   }
-  const lang = path.split('=');
+  const lang = path.split('lang=');
   if (lang !== '/') {
-    return lang[1];
+    return lang[1].substring(0, 2);
   }
   return null;
 };
